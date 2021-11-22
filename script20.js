1bc7ac793ed3fb90b0fa592920381a4eae6bcffe
djtoler <84260551+djtoler@users.noreply.github.com>
2021-11-22 14:42:03 -0500

    araay

---
+++ script20.js
@@ -0,0 +1,12 @@
+let animalArray = [
+    "tiger", "turtle", "bird"
+  ];
+
+function listArray () {
+    console.log(animalArray);
+    for (let i = 0; i < animalArray; i++) {
+        console.log(animalArray);
+        return[i];
+    }
+}
+console.log(listArray());
